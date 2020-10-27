df46d374a26b2e3a95c10b8238c677efb51bf740
Takashi Ikki <takahashi@omicale.com>
2020-10-28 01:22:01 +0900

    websocket対応

--- go-src/app/client.go
+++ go-src/app/client.go
@@ -16,19 +16,20 @@ type client struct {
 
 func (c *client) read() {
 	for {
-		if _, msg, err := c.socket.ReadMessage(); err == nil {
-			c.room.forward <- msg
-		} else {
-			break
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
 		}
+		c.room.forward <- msg
 	}
 	c.socket.Close()
 }
 
 func (c *client) write() {
 	for msg := range c.send {
-		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
-			break
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
 		}
 	}
 	c.socket.Close()
